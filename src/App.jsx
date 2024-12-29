0993cfbe656422585884956e7b746777bb626562
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2024-12-29 11:02:34 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Toasts from './Toasts'
+import { ToastProvider } from './ToastContext'
 import CharacterGeneration from "./widgets/CharGen/CharacterGeneration";
 
 function App() {
   return (
-    <>
+    <ToastProvider>
+      <div>
+        <Toasts />
+      </div>
       <CharacterGeneration />
-    </>
+    </ToastProvider>
   );
 }
 
