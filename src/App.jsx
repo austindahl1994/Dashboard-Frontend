631de48f8628518a320d97258fd92a44eb093a02
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2024-12-29 10:13:02 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import CharacterGeneration from "./widgets/CharGen/CharacterGeneration";
 
 function App() {
-  //create a state that saves the name and properties of profile
-  //create a frontend component that calls CharacterGeneration component, but 
-  //returns the component, holds the object to be returned with name?
-
   return (
     <>
       <CharacterGeneration />
