246fd827edb73b8be7c3b53902dd531c917de254
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-03-30 14:08:39 -0500

    Update ExpenseTracker.jsx

--- src/widgets/ExpenseTracker/ExpenseTracker.jsx
+++ src/widgets/ExpenseTracker/ExpenseTracker.jsx
@@ -151,7 +151,7 @@ const ExpenseTracker = () => {
         updateFn={updateFileData}
         deleteFn={removeFileData}
       />
-      {totals[0]?.amount !== 0 && <ExpenseTable categories={categories} totals={totals} />}
+      {(fileData && fileData.length > 0) && <ExpenseTable categories={categories} totals={totals} />}
       <Button onClick={() => setShowModal(true)}>Show Modal</Button>
       <CategorizeModal showModal={showModal} setShowModal={setShowModal} />
     </div>
