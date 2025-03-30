d46c56bd54a58e4a47e34a81ff0b2d8a4ad929c7
Austin Dahl <105532286+austindahl1994@users.noreply.github.com>
2025-03-30 11:22:31 -0500

    Update ExpenseTable.jsx
    First iteration of expense table

--- src/widgets/ExpenseTracker/ExpenseTable.jsx
+++ src/widgets/ExpenseTracker/ExpenseTable.jsx
@@ -1,15 +1,61 @@
 import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+const ExpenseTable = ({categories, totals}) => {
+  const [simpleTotals, setSimpleTotals] = useState({})
+  useEffect(() => {
+    const newTotalsObj = totals.reduce((acc, obj) => {
+      acc[obj?.subCategory] = obj.amount || 0
+      return acc
+    }, {})
+    setSimpleTotals(newTotalsObj)
+  }, [totals])
 
-const ExpenseTable = ({categories, subcategories}) => {
+  const categorySum = (subCatSet) => {
+    return Array.from(catObj.subCategory).reduce((acc, subCatStr) => {
+      return acc + simpleTotals[subCatStr]
+    }, 0)
+  }
   
   return (
-    <div>ExpenseTable</div>
+    <div>
+      <h1>ExpenseTable</h1>
+      <table>
+        <thead>
+          <tr>
+            {categories.map((catObj, catIndex) => (
+              <th key={catIndex} colSpan="2">{catObj.category}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+        {categories.map((catObj, catIndex) => (
+          <tr key={catIndex}>
+          {Array.from(catObj.subCategory).map((setStr, strIndex) => (
+            <React.Fragment key={strIndex}>
+              <td>{setStr}</td>
+              <td>{simpleTotals[setStr] || 0}</td>
+            </React.Fragment>
+          ))}
+          </tr>
+        ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            {/*In a single row, iterate through categories, for every category have a td of the sum*/}
+            {categories.map((catObj, catIndex) => (
+              <td colSpan="2" key={catIndex}>{categorySum(catObj.subCategory)}</td>
+            ))}
+          </tr>
+        </tfoot>
+      </table>
+    </div>
   )
 }
-
+//[{category: "Other", subCategory: Set["Unknown"]}]
 ExpenseTable.propTypes = {
     categories: PropTypes.array,
-    subcategories: PropTypes.array
+    subcategories: PropTypes.array,
+    totals: PropTypes.array
 }
 
 export default ExpenseTable
