78427584d270172b44c36e75daf9bf0e9f47608a
Austin Dahl <austindahl94@gmail.com>
2026-02-26 23:13:24 -0600

    a small update

--- src/bingo/home/BingoHome.tsx
+++ src/bingo/home/BingoHome.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Image } from "react-bootstrap";
 
-const BingoHome = () => {
+const DEFAULT_SRC =
+  "https://cabbage-bounty.s3.us-east-2.amazonaws.com/bingo/BingoHomescreen.png";
+const ALT_SRC =
+  "https://cabbage-bounty.s3.us-east-2.amazonaws.com/bingo/BingoHomeAlt.png";
+
+const BingoHome: React.FC = () => {
+  const [showAlt, setShowAlt] = useState(false);
+  const hoverTimer = useRef<number | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (hoverTimer.current) window.clearTimeout(hoverTimer.current);
+    };
+  }, []);
+
+  const startHoverTimer = () => {
+    if (hoverTimer.current) window.clearTimeout(hoverTimer.current);
+    hoverTimer.current = window.setTimeout(() => setShowAlt(true), 3000);
+  };
+
+  const clearHoverTimer = () => {
+    if (hoverTimer.current) window.clearTimeout(hoverTimer.current);
+    hoverTimer.current = null;
+    setShowAlt(false);
+  };
+
   return (
     <div className="d-flex w-100 h-100 m-0 p-0 justify-content-center align-items-center">
       <div
@@ -12,9 +37,14 @@ const BingoHome = () => {
           justifyContent: "center",
           alignItems: "center",
         }}
+        onMouseEnter={startHoverTimer}
+        onMouseLeave={clearHoverTimer}
+        onFocus={startHoverTimer}
+        onBlur={clearHoverTimer}
+        tabIndex={0}
       >
         <Image
-          src="https://cabbage-bounty.s3.us-east-2.amazonaws.com/bingo/BingoHomescreen.png"
+          src={showAlt ? ALT_SRC : DEFAULT_SRC}
           alt="Bingo homescreen"
           style={{ width: "100%", height: "100%", objectFit: "contain" }}
         />
